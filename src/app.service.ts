383330dfa97a935d6b0a0fd7a3bfff8ecd7e7fd9
vivinapriyanti <vivin.apriyanti@flash-coffee.com>
2022-01-04 09:07:20 +0700

    refactor(datadog): add comment datadog via api call

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
+// Using local utils to send datadog via api call
 import ddlog from './utils/datadog/ddlog';
 
 @Injectable()
 export class AppService {
   checkHealth(): any {
+    // Using local utils to send datadog via api call
     ddlog.info('[BE] Demo Datadog API Call', {
       color: 'black',
       time: { day: 4, month: 1, year: 2022 },
